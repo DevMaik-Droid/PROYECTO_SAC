347d85ce20b8443a0d677950a30d2aa47ecdd312
DevMaik <miguel.040.net@gmail.com>
2024-07-02 22:41:44 -0400

    dd
    dd

--- WEB/Backend/ventas/src/main/java/com/venta/ventas/controllers/ProductoController.java
+++ WEB/Backend/ventas/src/main/java/com/venta/ventas/controllers/ProductoController.java
@@ -5,14 +5,12 @@ import com.venta.ventas.models.Producto;
 import com.venta.ventas.services.ServiceProducto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/producto")
 public class ProductoController {
